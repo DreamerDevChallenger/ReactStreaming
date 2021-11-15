ef0230dfd0fdb496ff67208bd44e182ddfed5218
Ilyas Boukhechem <ilyas.boukhechem@gmx.fr>
2021-11-15 17:46:56 +0100

    display movies

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import "../Css/moviesList.css";
 import axios from "axios";
 
 function MoviesList() {
@@ -15,14 +15,18 @@ function MoviesList() {
   }, []);
 
   return (
-    <div>
-      {movies.map((m) => (
-        <li key={m._id}>
-          <img src={m._img} alt={m.title} />
-          <h3>{m.title}</h3>
-        </li>
-      ))}
-    </div>
+    <section>
+      <ul>
+        {movies.map((m) => (
+          <li key={m._id}>
+            <div className="picture">
+              <img src={m._img} alt={m.title} />
+            </div>
+            <h3>{m.title}</h3>
+          </li>
+        ))}
+      </ul>
+    </section>
   );
 }
 export default MoviesList;
